b8fabd65000f6b4ccd4aafa265dc69e1023c2905
Matt Porritt <matt.porritt@moodle.com>
2023-12-20 17:16:29 +1100

    initial updates to ai class

---
+++ classes/ai.php
@@ -0,0 +1,279 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This is the external API for this component.
+ *
+ * @package    local_assist
+ * @copyright   2023 Matt Porritt <matt.porritt@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_assist;
+
+use core\http_client;
+
+/**
+ * Process AI API calls and generate content responses.
+ *
+ * @copyright   2023 Matt Porritt <matt.porritt@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class ai {
+    // API key.
+    private string $apikey;
+
+    // API org id.
+    private string $orgid;
+
+    // API endpoint.
+    private string $aiendpoint = 'https://api.openai.com/v1/chat/completions';
+
+    // HTTP Client.
+    private http_client $client;
+
+    // AI model.
+    private string $model = 'gpt-4';
+
+    // AI "personality" options.
+    private array $personalityoptions = [
+        0 => 'You are a lecturer at a higher education university',
+        1 => 'You are a postgraduate lecturer at a university',
+        3 => 'You are a high school teacher',
+        4 => 'You are a primary school teacher',
+        5 => 'You are a topic matter expert in an organisation or business',
+    ];
+
+    // AI personality.
+    private string $personality;
+
+    // AI temperature. Between 0 and 2.
+    private float $temperature = 0.3;
+
+    // Array to define supported AI operations.
+    private static array $supportedoperations = [
+        'summarise' => true,
+        'explain' => true,
+        'questionsask' => false,
+        'questionsanswer' => true,
+        'translate' => true,
+        'images' => false,
+    ];
+
+    private array $aiactions = [
+        'explain' => 'Explain the provided text. Clarify complex concepts or terms.',
+        'summarise' => 'Summarise the provided text. Condense long text into key points.',
+        'translate' => 'Translate the provided text into another language.',
+        'questionanswer' => 'Answer questions about the provided text.',
+    ];
+
+    /**
+     * Class constructor.
+     */
+    public function __construct() {
+        // Get api key from config.
+        $this->apikey = get_config('local_assist', 'apikey');
+        // Get api org id from config.
+        $this->orgid = get_config('local_assist', 'orgid');
+        // Get personality from config.
+        $this->personality = $this->personalityoptions[get_config('local_assist', 'personality')];
+        // Create http client.
+        $this->client = new http_client([
+            'base_uri' => $this->aiendpoint,
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $this->apikey,
+                'OpenAI-Organization' => $this->orgid,
+            ]
+        ]);
+    }
+
+    /**
+     * Get the supported AI operations.
+     *
+     * @return array The supported operations.
+     */
+    public static function get_supported_operations(): array {
+        return self::$supportedoperations;
+    }
+
+    /**
+     * Generate content from the AI service.
+     *
+     * @param string $prompttext The prompt text.
+     * @param int $contextid The context id.
+     * @return array The generated content.
+     */
+    public function generate_content(string $prompttext, int $contextid): array {
+        global $USER;
+
+
+        // Check rate limiting for user before continuing.
+        // If rate limit is exceeded, return an error response.
+        if (!ratelimiter::is_request_allowed($USER->id)){
+            return [
+                'errorcode' => 429,
+                'error' => 'User rate limit exceeded.',
+            ];
+        }
+
+        // Update temperature.
+        $this->update_temperature($prompttext, $contextid, $USER->id);
+
+        // Get request object.
+        $requestobj = $this->generate_request_object($prompttext);
+
+        // Get response from AI service.
+        $responsearr = $this->query_ai_api($requestobj);
+        if (!isset($responsearr['errorcode'])) {
+            $responsearr['prompttext'] = $prompttext;
+        }
+
+        return $responsearr;
+    }
+
+    /**
+     * Generate request object ready to send to the AI service.
+     *
+     * @param string $prompttext The prompt text.
+     */
+    private function generate_request_object(string $prompttext): \stdClass {
+        // Create the AI request object.
+        $systemobj = new \stdClass();
+        $systemobj->role = 'system';
+        $systemobj->content = $this->personality;
+
+        $userobj = new \stdClass();
+        $userobj->role = 'user';
+        $userobj->content = $prompttext;
+
+        $requestobj = new \stdClass();
+        $requestobj->model = $this->model;
+        $requestobj->temperature = $this->temperature;
+        $requestobj->messages = [$systemobj, $userobj];
+
+        return $requestobj;
+    }
+
+    /**
+     * Get the current temperature for the AI service.
+     *
+     * @param string $prompttext The prompt text.
+     * @param int $contextid The context id.
+     * @param int $userid The user id.
+     * @return void
+     */
+    private function update_temperature(string $prompttext, int $contextid, int $userid): void {
+        // Set up the cache API for the Tiny AI Plugin.
+        $cache = \cache::make('local_assist', 'request_temperature');
+        $cachekeystr = $prompttext . (string)$contextid . (string)$userid;
+        $cachekey = hash_pbkdf2('sha3-256', $cachekeystr, 'local_assist', 1);
+
+        // Check cache for existing response.
+        // If response is a hit then a response has already been generated for this prompt,
+        // and we increase the temperature to generate a new response.
+        if ($cache->get($cachekey)) {
+            $this->temperature = $cache->get($cachekey) + 0.1;
+        }
+
+        // Max allowed temperature is 2.
+        if ($this->temperature > 2) {
+            $this->temperature = 2;
+        }
+
+        // Update the cache.
+        $cache->set($cachekey, $this->temperature);
+    }
+
+    /**
+     * Query the AI service.
+     *
+     * @param \stdClass $requestobj The request object.
+     * @return array The response from the AI service.
+     * @param ?http_client $client The http client.
+     */
+    private function query_ai_api(\stdClass $requestobj, ?http_client $client = null): array {
+        // Allow for dependency injection of http client.
+        if ($client) {
+            $this->client = $client;
+        }
+
+        // Create the AI request object.
+        $requestjson = json_encode($requestobj);
+
+        // Call the external AI service.
+        $response = $this->client->request('POST', '', [
+                'body' => $requestjson,
+        ]);
+
+        // Handle the various response codes.
+        $status = $response->getStatusCode();
+        if ($status == 200) {
+            return $this->handle_api_success($response);
+        } else {
+            return $this->handle_api_error($status, $response);
+        }
+    }
+
+    /**
+     * Handle an error from the external AI api.
+     *
+     * @param int $status The status code.
+     * @param \GuzzleHttp\Psr7\Response $response The response object.
+     * @return array The error response.
+     */
+    private function handle_api_error(int $status, \GuzzleHttp\Psr7\Response $response): array {
+
+        if ($status == 500) {
+            $responsearr = [
+                'errorcode' => $status,
+                'error' => 'Internal server error.',
+            ];
+        } else if ($status == 503) {
+            $responsearr = [
+                'errorcode' => $status,
+                'error' => 'Service unavailable.',
+            ];
+        } else {
+            $responsebody = $response->getBody();
+            $bodyobj = json_decode($responsebody->getContents());
+            $responsearr =[
+                'errorcode' => $status,
+                'error' => $bodyobj->error->message,
+            ];
+        }
+
+        return $responsearr;
+    }
+
+    /**
+     * Handle a successful response from the external AI api.
+     *
+     * @param \GuzzleHttp\Psr7\Response $response The response object.
+     * @return array The response.
+     */
+    private function handle_api_success(\GuzzleHttp\Psr7\Response $response): array {
+        $responsebody = $response->getBody();
+        $bodyobj = json_decode($responsebody->getContents());
+
+        return [
+            'model' => $this->model,
+            'personality' => $this->personality,
+            'generateddate' => time(),
+            'generatedcontent' => $bodyobj->choices[0]->message->content,
+        ];
+    }
+}
---
+++ tests/ai_test.php
@@ -0,0 +1,141 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+use core\http_client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use local_assist\ai;
+
+/**
+ * Unit tests for the ai class in the local_assist plugin.
+ *
+ * @package    local_assist
+ * @category   test
+ * @copyright   2023 Matt Porritt <matt.porritt@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @coversDefaultClass \local_assist\ai
+ */
+class ai_test extends \advanced_testcase {
+
+    /**
+     * Test the generate content function.
+     *
+     * @since  Moodle 4.2
+     * @return void
+     */
+    public function test_generate_content() {
+        $this->resetAfterTest(true);
+        $prompttext = 'Provide a brief introduction to a cloud computing course.';
+        set_config('apikey', 'abc123', 'local_assist');
+        set_config('orgid', 'abc123', 'local_assist');
+
+        $mock = new MockHandler([
+                new Response(200,),
+                new Response()
+        ]);
+        $client = new \core\http_client(['mock' => $mock]);
+
+        $ai = new ai();
+        //$result = $ai->generate_content($prompttext);
+    }
+
+    /**
+     * Test the API success response handler method.
+     *
+     * @since  Moodle 4.2
+     * @return void
+     * @covers local_assist\ai::handle_api_success
+     */
+    public function test_handle_api_success() {
+        $responsebodyjson = <<<EOD
+{
+   "id":"chatcmpl-7AwwHl4wmVXd0FaBn1fgWs3MSeIiV",
+   "object":"chat.completion",
+   "created":1682844197,
+   "model":"gpt-4-0314",
+   "usage":{
+      "prompt_tokens":9,
+      "completion_tokens":9,
+      "total_tokens":18
+   },
+   "choices":[
+      {
+         "message":{
+            "role":"assistant",
+            "content":"Hello! How can I help you today?"
+         },
+         "finish_reason":"stop",
+         "index":0
+      }
+   ]
+}
+EOD;
+        $response = new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                $responsebodyjson
+        );
+
+        // We're testing a private method, so we need to setup reflector magic.
+        $ai = new ai();
+        $method = new ReflectionMethod('\local_assist\ai', 'handle_api_success');
+        $method->setAccessible(true); // Allow accessing of private method.
+
+        $result = $method->invoke($ai, $response);
+
+        $this->assertEquals('Hello! How can I help you today?', $result['generatedcontent']);
+        $this->assertEquals('gpt-4', $result['model']);
+        $this->assertEquals('You are an undergraduate Lecturer at a university', $result['personality']);
+    }
+
+    /**
+     * Test the API error response handler method.
+     *
+     * @since  Moodle 4.2
+     * @return void
+     * @covers local_assist\ai::handle_api_error
+     */
+    public function test_handle_api_error() {
+        $responses = [
+                500 => new Response(500, ['Content-Type' => 'application/json']),
+                503 => new Response(503, ['Content-Type' => 'application/json']),
+                401 => new Response(401, ['Content-Type' => 'application/json'],
+                        '{"error": {"message": "Invalid Authentication"}}'),
+                404 => new Response(404, ['Content-Type' => 'application/json'],
+                        '{"error": {"message": "You must be a member of an organization to use the API"}}'),
+                429 => new Response(429, ['Content-Type' => 'application/json'],
+                        '{"error": {"message": "Rate limit reached for requests"}}'),
+        ];
+
+        $ai = new ai();
+        $method = new ReflectionMethod('\local_assist\ai', 'handle_api_error');
+        $method->setAccessible(true); // Allow accessing of private method.
+
+        foreach($responses as $status => $response) {
+            $result = $method->invoke($ai, $status, $response);
+            $this->assertEquals($status, $result['errorcode']);
+            if ($status == 500) {
+                $this->assertEquals('Internal server error.', $result['error']);
+            } else if ($status == 503) {
+                $this->assertEquals('Service unavailable.', $result['error']);
+            } else {
+                $this->assertStringContainsString($response->getBody()->getContents(), $result['error']);
+            }
+        }
+    }
+}
